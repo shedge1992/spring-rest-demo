a8717a4d1fdf5e8a9ce78c6c8b0142617896fc7e
deepakshedge <shedge.deepak1992@gmail.com>
2021-08-24 01:32:52 +0530

    Spring Rest understanding

---
+++ src/main/java/com/dshedge/spring/SpringBootRestApplication.java
@@ -0,0 +1,12 @@
+package com.dshedge.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApplication.class, args);
+	}
+}
